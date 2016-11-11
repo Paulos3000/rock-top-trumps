27d137b8ebe37927bbcb17e5983a6748b08992f7
Paul Child <pauljchild@gmail.com>
2016-11-11 19:49:11 +0000

    delete comments in store

--- abramov-redux-fun/public/store.js
+++ abramov-redux-fun/public/store.js
@@ -12,11 +12,3 @@ const enhancers = compose( // this method is used to add 'enhancers' to the stor
 // Define 'defaultState'? Pass as 2nd argument to 'store'
 export const store = createStore(rootReducer, enhancers);
 export const history = syncHistoryWithStore(browserHistory, store);
-
-// // Add Hot Reloading for reducers... --> why not working?
-// if (module.hot) {
-//    module.hot.accept('./reducers/', () => {
-//       const nextRootReducer = require('./reducers/reducer-index').default;
-//       store.replaceReducer(nextRootReducer);
-//    })
-// }
