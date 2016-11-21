c5e952cd4fb0dc363e04c532968e4e9f97c855b6
Paul Child <pauljchild@gmail.com>
2016-11-21 22:13:23 +0000

    tidy-up

--- abramov-redux-fun/public/util/getVisibleTodos.js
+++ abramov-redux-fun/public/util/getVisibleTodos.js
@@ -1,17 +1,17 @@
-// FUNCTION - filter todos by action type (SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETE)
-export const getVisibleTodos = (todos, filter) => {
-   switch(filter) {
-      case 'all' :
-         return todos;
-      case 'completed' :
-         return todos.filter(
-            todo => todo.completed
-         )
-      case 'active' :
-         return todos.filter(
-            todo => !todo.completed
-         )
-      default :
-         throw new Error(`Unknown filter: ${filter}`)
-   }
-}
+// // FUNCTION - filter todos by action type (SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETE)
+// export const getVisibleTodos = (todos, filter) => {
+//    switch(filter) {
+//       case 'all' :
+//          return todos;
+//       case 'completed' :
+//          return todos.filter(
+//             todo => todo.completed
+//          )
+//       case 'active' :
+//          return todos.filter(
+//             todo => !todo.completed
+//          )
+//       default :
+//          throw new Error(`Unknown filter: ${filter}`)
+//    }
+// }
