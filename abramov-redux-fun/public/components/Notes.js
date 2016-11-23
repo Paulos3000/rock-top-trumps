9138e596303cf15f90c4fe4d27e7f2d752578868
Paul Child <pauljchild@gmail.com>
2016-11-23 21:23:42 +0000

    update notes

--- abramov-redux-fun/public/components/Notes.js
+++ abramov-redux-fun/public/components/Notes.js
@@ -2,15 +2,10 @@ import React from 'react'
 
 const Notes = () => (
    <div>
+      <h4>#13, Redux: Adding a fake backend.</h4>
       <ul>
-         <li>#11, Normalising State Shape: tricky, but it works. Maybe go over again.</li>
-         <li>#12, Wrapping dispatch() to log functions: basically a manual Redux Devtools. Maybe check out console.group() api, plus console.log styling.</li>
-      </ul>
-
-      <hr/>
-
-      <ul>
-         <li>#13, Redux: Adding a fake backend. </li>
+         <li>Note 1</li>
+         <li>Note 2</li>
       </ul>
    </div>
 )
