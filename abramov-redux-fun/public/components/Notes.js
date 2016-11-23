ef1dc85280e95d2369ca63340a9777be8fd43443
Paul Child <pauljchild@gmail.com>
2016-11-23 18:34:27 +0000

    finished first half

---
+++ abramov-redux-fun/public/components/Notes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Notes = () => (
+   <div>
+      <ul>
+         <li>#11, Normalising State Shape: tricky, but it works. Maybe go over again.</li>
+         <li>#12, Wrapping dispatch() to log functions: basically a manual Redux Devtools. Maybe check out console.group() api, plus console.log styling.</li>
+      </ul>
+
+      <hr/>
+
+      <ul>
+         <li>#13, Redux: Adding a fake backend. </li>
+      </ul>
+   </div>
+)
+
+export default Notes
--- abramov-redux-fun/public/components/TodoApp.js
+++ abramov-redux-fun/public/components/TodoApp.js
@@ -4,16 +4,18 @@ import React, { Component } from 'react'
 import AddTodo from './AddTodo'
 import VisibleTodoList from './VisibleTodoList'
 import { Footer } from './Footer'
+import Notes from './Notes'
 
 import { v4 } from 'node-uuid'
 
 // MAIN CONTAINER COMPONENT
 export const TodoApp = ({ params }) => (
    <div className='container'>
-
       <h2>Todo App</h2>
-      <h2><small>Finished #11 - tricky, but it works. Maybe go over again.</small></h2>
-      <hr/>
+      <hr />
+
+      <Notes />
+      <hr />
 
       <AddTodo />
       <VisibleTodoList />
