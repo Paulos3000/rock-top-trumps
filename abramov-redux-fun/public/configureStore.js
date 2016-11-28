f401b61dc3b2b3952d9a98392eedfd4580e9b099
Paul Child <pauljchild@gmail.com>
2016-11-28 10:57:02 +0000

    npm install middleware packages so manual implementation no longer needed

--- abramov-redux-fun/public/configureStore.js
+++ abramov-redux-fun/public/configureStore.js
@@ -1,57 +1,58 @@
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import todoApp from './reducers/index'
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
+import { Provider } from 'react-redux';
 import throttle from 'lodash/throttle';
 
-const logger = (store) => (next) => {
-   if (!console.group) {
-      return next;
-   }
-   return (action) => {
-      console.group(action.type);
-      console.log('%c previous state', 'color: gray', store.getState())
-      console.log('%c action', 'color: blue', action);
-      const returnValue = next(action);
-      console.log('%c next state', 'color: green', store.getState());
-      console.groupEnd(action.type);
-      return returnValue;
-   }
-}
+import todoApp from './reducers/index';
 
+// ------------------------------------------------
+// OLD MANUAL LOGGER IMPLEMENTATION (FOR REFERENCE)
 
-const promise = (store) => (next) => (action) => {
-   // if it is a PROMISE...
-   if (typeof action.then === 'function') {
-      // return the action, wait for it resolve, then dispatch the synchronous action on that.
-      return action.then(next);
-   }
-   // otherwise just call dispatch with action object, as normal (synchronous)
-   return next(action)
-};
+// const logger = (store) => (next) => {
+//    if (!console.group) {
+//       return next;
+//    }
+//    return (action) => {
+//       console.group(action.type);
+//       console.log('%c previous state', 'color: gray', store.getState())
+//       console.log('%c action', 'color: blue', action);
+//       const returnValue = next(action);
+//       console.log('%c next state', 'color: green', store.getState());
+//       console.groupEnd(action.type);
+//       return returnValue;
+//    }
+// }
 
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-   // run code for every middleware
-   middlewares.slice().reverse().forEach(middleware =>
-      // re-write store.dispatch as before, but with each middleware function in array (see above for details)
-      store.dispatch = middleware(store)(store.dispatch)
-   )
-}
+// OLD MANUAL PROMISE IMPLEMENTATION (FOR REFERENCE)
+
+// const promise = (store) => (next) => (action) => {
+//    // if it is a PROMISE...
+//    if (typeof action.then === 'function') {
+//       // return the action, wait for it resolve, then dispatch the synchronous action on that.
+//       return action.then(next);
+//    }
+//    // otherwise just call dispatch with action object, as normal (synchronous)
+//    return next(action)
+// };
+// ------------------------------------------------
 
 const configureStore = () => {
-   const store = createStore(todoApp);
    const middlewares = [promise];
-
    // if in DEVELOPMENT mode, enable logging
    if (process.env.NODE_ENV !== 'production') {
-      middlewares.push(logger);
+      middlewares.push(createLogger());
    }
-
    middlewares.push(promise);
 
-   wrapDispatchWithMiddlewares(store, middlewares);
+   // wrapDispatchWithMiddlewares(store, middlewares);
 
-   // return store to app.
-   return store;
+   return createStore(
+      todoApp,
+      // persistedState, --> Can add this if needed. !Must be before enhancer!
+      // last argument = 'enhancer' (optional)
+      applyMiddleware(...middlewares)
+   );
 }
 
 export default configureStore;
