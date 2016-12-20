4e16c5d5ee6a68d1306a21ba0e02b71f244644ca
Paul Child <pauljchild@gmail.com>
2016-12-20 08:56:39 +0000

    streamline Home more

--- fresh-redux/public/components/Home.js
+++ fresh-redux/public/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => (
       <hr />
       <div className='jumbotron'>
          <p><small>This is a test React application using async Redux actions to gather data from a server and present in the view. Styled with Bootstrap.</small></p>
-         <p><small>All data fetched from <code>https://jsonplaceholder.typicode.com/</code></small></p>
+         <p><small>All data fetched from <code>jsonplaceholder.typicode.com</code></small></p>
          <p><small>Work in progress...</small></p>
       </div>
    </div>
