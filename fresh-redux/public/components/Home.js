6fff0c56d44aafe0df76a121a129f14c4b95a424
Paul Child <pauljchild@gmail.com>
2016-12-20 08:54:22 +0000

    reduce Home text size

--- fresh-redux/public/components/Home.js
+++ fresh-redux/public/components/Home.js
@@ -5,9 +5,9 @@ const Home = () => (
       <h1>Home</h1>
       <hr />
       <div className='jumbotron'>
-         <p>This is a test React application using async Redux actions to gather data from a server and present in the view. Styled with Bootstrap.</p>
-         <p>All data fetched from <code>https://jsonplaceholder.typicode.com/</code></p>
-         <p>Work in progress...</p>
+         <p><small>This is a test React application using async Redux actions to gather data from a server and present in the view. Styled with Bootstrap.</small></p>
+         <p><small>All data fetched from <code>https://jsonplaceholder.typicode.com/</code></small></p>
+         <p><small>Work in progress...</small></p>
       </div>
    </div>
 )
