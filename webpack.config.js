7032927cf398c6c151e4f41001673d88988677aa
Paul Child <pauljchild@gmail.com>
2017-01-08 13:56:36 +0000

    changed source-map

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    devtool: 'eval-source-map',
+   //  devtool: 'eval-source-map',
+    devtool: 'source-map',
     entry: path.join(__dirname, 'public', 'app-client.js'),
     devServer: {
       inline: true,
